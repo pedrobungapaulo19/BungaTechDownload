a91055b741f558f4f83dd6e99cf429c224dc980a
Pedro Bunga Paulo <pedrobungapaulo2023@gmail.com>
2025-11-09 22:39:20 -0300

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -5,13 +5,7 @@ import { getAuth, GoogleAuthProvider, signInWithPopup } from "https://www.gstati
 import { getStorage } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-storage.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAViL6k3FmjtvDgQjO9lRKZ55zYTZE3lW4",
-  authDomain: "bunga-tech-site-de-download.firebaseapp.com",
-  projectId: "bunga-tech-site-de-download",
-  storageBucket: "bunga-tech-site-de-download.firebasestorage.app",
-  messagingSenderId: "75070741998",
-  appId: "1:75070741998:web:2874453d8a21aae5f242ff"
-};
+  
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
